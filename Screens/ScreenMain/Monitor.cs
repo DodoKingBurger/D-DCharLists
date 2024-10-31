0b9c0547a4b1369816f6ff913cd42a98f47f617c
DodoKingBurger <vasilevykhd@icloud.com>
2024-10-31 15:42:07 +0400

    Корявые ппытки в проект Пытки и промывки

--- Screens/ScreenMain/Monitor.cs
+++ Screens/ScreenMain/Monitor.cs
@@ -17,81 +17,26 @@ namespace D_DCharList.Screens
 
 		private Enum _choosenPoint;
 
-		private ShowMenusCursor _showMenusCursor;
-		private IScreen _screenActionsWithSheet;
-		private IScreen _screenCreateSheet;
-		private IScreen _screenLoadSheet;
 
 		public Monitor()
 		{
-			_showMenusCursor = new ShowMenusCursor();
-			_screenActionsWithSheet = new ScreenActionsWithSheet();
-			_screenCreateSheet = new ScreenCreateSheet();
-			_screenLoadSheet = new ScreenLoadSheet();
+
 		}
 
 		public void ShowScreen(EnumMainMenuTitles List)
 		{
-			_choosenPoint = _showMenusCursor.ShowMenuPoints(EnumMainMenuTitles.MainMenu, typeof(EnumMainMenuPoints));
-			switch (_choosenPoint)
-			{
-				case EnumMainMenuPoints.CreateSheet:
-					_screenCreateSheet.ShowScreen();
-					_screenActionsWithSheet.ShowScreen();
-					_isSheetLoaded = true;
-					break;
 
-				case EnumMainMenuPoints.LoadSheet:
-					_screenLoadSheet.ShowScreen();
-					_isSheetLoaded = true;
-					break;
-
-				case EnumMainMenuPoints.SaveSheeet:
-					if (_isSheetLoaded == true)
-					{
-						SaveSheet();
-					}
-					else
-					{
-						PrintListNotLoaded();
-					}
-					break;
-
-				case EnumMainMenuPoints.SheetActions:
-					if (_isSheetLoaded == true)
-					{
-						_screenActionsWithSheet.ShowScreen();
-					}
-					else
-					{
-						PrintListNotLoaded();
-					}
-					break;
-
-				case EnumMainMenuPoints.Exit:
-					Environment.Exit(0);
-					break;
-
-				default:
-					PrintListNotLoaded();
-					break;
-			}
 
 		}
 
 		private void PrintListNotLoaded()
 		{
-			Console.Clear();
-			Console.WriteLine(LocalizationsStash.SelectedLocalization[EnumMainMenuTitles.FirstLoadOrCreateSheet]);
-			Console.ReadKey();
+
 		}
 
 		private void SaveSheet()
 		{
-			CurrentHeroSheet.SaveSheet();
-			Console.Clear();
-			Console.WriteLine(LocalizationsStash.SelectedLocalization[EnumMainMenuTitles.SheetSaved]);
-			Console.ReadKey();
+
 		}
 	}
 }
